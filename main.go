3e8ba1df1b61f1e176f0ccedcb1da79fac861250
Manuel Carbajal <mcarbajal@blumonpay.com.mx>
2021-03-08 18:34:39 -0600

    Se colocan endpoint en los archivos html. Se agrega metodo para servir archivos estaticos

--- main.go
+++ main.go
@@ -11,7 +11,18 @@ import (
 func main() {
 	r := mux.NewRouter()
 	web.RegistryHandlers(r)
-	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
+
+	registryStaticHandlers(r)
+
 	fmt.Println("Servidor iniciado")
 	http.ListenAndServe(":8000", r)
+}
+
+func registryStaticHandlers(r *mux.Router) {
+
+	r.PathPrefix("/css/").Handler(http.StripPrefix("/css/", http.FileServer(http.Dir("./static/css"))))
+	r.PathPrefix("/assets/").Handler(http.StripPrefix("/assets/", http.FileServer(http.Dir("./static/assets"))))
+	r.PathPrefix("/images/").Handler(http.StripPrefix("/images/", http.FileServer(http.Dir("./static/images"))))
+	r.PathPrefix("/scripts/").Handler(http.StripPrefix("/scripts/", http.FileServer(http.Dir("./static/scripts"))))
+
 }

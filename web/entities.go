a3b89f443c08474eb1234ae1391653879d274cab
Manuel Carbajal <manicar2093@gmail.com>
2021-03-09 00:51:10 -0600

    Se agregan entidades de la base de datos

--- web/entities.go
+++ web/entities.go
@@ -1,4 +1,32 @@
 package web
 
+import (
+	"database/sql"
+	"time"
+)
+
+// User es la representación de un registro en la base de datos
 type User struct {
+	UserID           int32
+	RolID            sql.NullInt32
+	Name             string
+	PaternalSureName string
+	MaternalSureName string
+	Email            string
+	Password         string
+	CreationDate     time.Time
+	EditDate         sql.NullTime
+	Status           bool
+}
+
+// DetailsHosting es la representación de un registro en la base de datos
+type DetailsHosting struct {
+	DetailsHostingID int32
+	UserID           int32
+	Host             string
+	SessionStart     time.Time
+	SessionClosure   sql.NullTime
+	CreationDate     time.Time
+	EditDate         sql.NullTime
+	Status           bool
 }

cf4f674589b1e13297e6fb662c456ab6e2377843
Manuel Carbajal <manicar2093@gmail.com>
2021-03-27 01:16:48 -0600

    Se agrega struct para el manejo de renderizado de templates

--- app/utils/templates.go
+++ app/utils/templates.go
@@ -1,19 +1,37 @@
 package utils
 
 import (
+	"errors"
 	"net/http"
 	"text/template"
 )
 
+var (
+	ErrExecution        = errors.New("error al ejecutar el template")
+	ErrTemplateNotFound = errors.New("template was not found")
+)
+
 type TemplateUtils interface {
+	// RenderTemplateToWriter renderiza el template en el path especificado. Si este no se encuentra el ResponseWriter se le colocará el status 404.
+	RenderTemplateToResponseWriter(templatePath string, w http.ResponseWriter, data interface{}) error
 }
 
-// RenderTemplateToWriter realiza el render del template que se encuentre en el path especificado
-func RenderTemplateToWriter(templatePath string, w http.ResponseWriter, data interface{}) error {
-	t := template.Must(template.ParseFiles(templatePath))
-	e := t.Execute(w, data)
+type TemplateUtilsImpl struct{}
+
+func NewTemplateUtils() TemplateUtils {
+	return &TemplateUtilsImpl{}
+}
+
+func (t TemplateUtilsImpl) RenderTemplateToResponseWriter(templatePath string, w http.ResponseWriter, data interface{}) error {
+	tpl, e := template.ParseFiles(templatePath)
 	if e != nil {
-		return e
+		Error.Printf("Error al parsear el template '%s': Detalles: \n\t%v", templatePath, e)
+		return ErrTemplateNotFound
+	}
+
+	if e = tpl.Execute(w, data); e != nil {
+		Error.Printf("Error al ejecutar el template '%s': Detalles: \n\t%v", templatePath, e)
+		return ErrExecution
 	}
 	return nil
 }

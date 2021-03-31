9ec065d4e929ed420a60f148c4d098e90ab62aab
Manuel Carbajal <manicar2093@gmail.com>
2021-03-31 00:15:16 -0600

    Se realiza TDD para todos los demas endpoint de users

--- app/controllers/user.controller.go
+++ app/controllers/user.controller.go
@@ -1,18 +1,22 @@
 package controllers
 
 import (
+	"database/sql"
 	"encoding/json"
 	"net/http"
+	"strconv"
 
+	"github.com/gorilla/mux"
 	"github.com/manicar2093/guianetThea/app/dao"
 	"github.com/manicar2093/guianetThea/app/models"
 	"github.com/manicar2093/guianetThea/app/services"
 	"github.com/manicar2093/guianetThea/app/utils"
 )
 
+// TODO: Realizar refactorización de código
 type UserController interface {
 	CreateUser(w http.ResponseWriter, r *http.Request)
-	GetUserByID(w http.ResponseWriter, r *http.Request)
+	//GetUserByID(w http.ResponseWriter, r *http.Request)
 	UpdateUser(w http.ResponseWriter, r *http.Request)
 	RestorePassword(w http.ResponseWriter, r *http.Request)
 	DeleteUser(w http.ResponseWriter, r *http.Request)
@@ -55,18 +59,127 @@ func (u UserControllerImpl) CreateUser(w http.ResponseWriter, r *http.Request) {
 	utils.JSON(nil, http.StatusCreated, w)
 }
 
-func (u UserControllerImpl) GetUserByID(w http.ResponseWriter, r *http.Request) {
+/*func (u UserControllerImpl) GetUserByID(w http.ResponseWriter, r *http.Request) {
 	panic("not implemented") // TODO: Implement
-}
+}*/
 
 func (u UserControllerImpl) UpdateUser(w http.ResponseWriter, r *http.Request) {
-	panic("not implemented") // TODO: Implement
+	var data models.UpdateUserData
+	if e := json.NewDecoder(r.Body).Decode(&data); e != nil {
+		utils.JSON(map[string]interface{}{"message": "No se logró obtener la información necesaria. Valida la documentación"}, http.StatusInternalServerError, w)
+		return
+	}
+	err, valid := u.validatorService.Validate(data)
+	if !valid {
+		utils.JSON(map[string]interface{}{
+			"message": "La información es incorrecta. Favor de revisar la documentación",
+			"errores": err,
+		}, http.StatusBadRequest, w)
+		return
+	}
+
+	stored, e := u.userDao.FindUserByID(int32(data.ID))
+	if e != nil {
+		switch {
+		case e == sql.ErrNoRows:
+			utils.JSON(map[string]interface{}{
+				"message": "No se encontro el usuario que se quiere editar",
+			}, http.StatusNotFound, w)
+			return
+		default:
+			utils.Error.Printf("Error inesperado al buscar el usuario. Detalles: \n\t%v", e)
+			utils.JSON(map[string]interface{}{
+				"message": "Hubo un error inesperado al buscar al usuario",
+			}, http.StatusInternalServerError, w)
+			return
+		}
+	}
+
+	stored.Name = data.Name
+	stored.PaternalSureName = data.PaternalSureName
+	stored.MaternalSureName.String = data.MaternalSureName
+	stored.Email = data.Email
+	stored.RolID.Int64 = int64(data.RolID)
+
+	e = u.userDao.Save(&stored)
+	if e != nil {
+		utils.Error.Printf("Error inesperado al gurdar el usuario. Detalles: \n\t%v", e)
+		utils.JSON(map[string]interface{}{
+			"message": "Hubo un error inesperado al editar el usuario",
+		}, http.StatusInternalServerError, w)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+
 }
 
 func (u UserControllerImpl) RestorePassword(w http.ResponseWriter, r *http.Request) {
-	panic("not implemented") // TODO: Implement
+	var data models.RestorePasswordData
+
+	if e := json.NewDecoder(r.Body).Decode(&data); e != nil {
+		utils.JSON(map[string]interface{}{"message": "No se logró obtener la información necesaria. Valida la documentación"}, http.StatusInternalServerError, w)
+		return
+	}
+
+	err, valid := u.validatorService.Validate(data)
+	if !valid {
+		utils.JSON(map[string]interface{}{
+			"message": "La información es incorrecta. Favor de revisar la documentación",
+			"errores": err,
+		}, http.StatusBadRequest, w)
+		return
+	}
+
+	stored, e := u.userDao.FindUserByID(int32(data.ID))
+	if e != nil {
+		switch {
+		case e == sql.ErrNoRows:
+			utils.JSON(map[string]interface{}{
+				"message": "No se encontro el usuario que se quiere editar",
+			}, http.StatusNotFound, w)
+			return
+		default:
+			utils.Error.Printf("Error inesperado al buscar el usuario. Detalles: \n\t%v", e)
+			utils.JSON(map[string]interface{}{
+				"message": "Hubo un error inesperado al buscar al usuario",
+			}, http.StatusInternalServerError, w)
+			return
+		}
+	}
+
+	stored.Password = data.Password
+
+	e = u.userDao.Save(&stored)
+	if e != nil {
+		utils.Error.Printf("Error inesperado al gurdar el usuario. Detalles: \n\t%v", e)
+		utils.JSON(map[string]interface{}{
+			"message": "Hubo un error inesperado al editar el usuario",
+		}, http.StatusInternalServerError, w)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+
 }
 
 func (u UserControllerImpl) DeleteUser(w http.ResponseWriter, r *http.Request) {
-	panic("not implemented") // TODO: Implement
+	idUser, e := strconv.Atoi(mux.Vars(r)["idUser"])
+
+	if e != nil {
+		utils.Error.Printf("Hubo un error al obtener el idUser. Detalles: \n\t%v", e)
+		utils.JSON(map[string]interface{}{
+			"message": "Quizá lo que enviaste en el path no es un numero. Favor de revisar la documentación",
+		}, http.StatusBadRequest, w)
+		return
+	}
+
+	e = u.userDao.Delete(int32(idUser))
+	if e != nil {
+		utils.Error.Printf("Hubo un error al eliminar el usuario. Detalles: \n\t%v", e)
+		utils.JSON(map[string]interface{}{
+			"message": "Hubo un problema inesperado al borrar al usuario",
+		}, http.StatusInternalServerError, w)
+		return
+	}
 }
--- app/controllers/user.controller_test.go
+++ app/controllers/user.controller_test.go
@@ -2,35 +2,32 @@ package controllers
 
 import (
 	"bytes"
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 
 	"github.com/gorilla/mux"
+	"github.com/manicar2093/guianetThea/app/entities"
 	"github.com/manicar2093/guianetThea/app/mocks"
 	"github.com/manicar2093/guianetThea/app/models"
 	"github.com/stretchr/testify/assert"
+	"gopkg.in/guregu/null.v4/zero"
 )
 
+// Mocks
 var (
 	router           *mux.Router
 	validatorService mocks.ValidatorServiceMock
 	userDao          mocks.UserDaoMock
 )
 
-func setUp() {
-	router = mux.NewRouter()
-	validatorService = mocks.ValidatorServiceMock{}
-	userDao = mocks.UserDaoMock{}
-}
-
-// TestCreateUser valida el happy path del proceso de guardado de un usuario
-func TestCreateUser(t *testing.T) {
-	setUp()
-	target := "/admin/user/registry"
-	data := map[string]interface{}{
+// Data for testing
+var (
+	creationData = map[string]interface{}{
 		"name":              "Test",
 		"paternal_surename": "TestPS",
 		"maternal_surename": "TestMS",
@@ -40,7 +37,7 @@ func TestCreateUser(t *testing.T) {
 		"password_confirm":  "pass1",
 	}
 
-	userCreationModel := models.CreateUserData{
+	userCreationModel = models.CreateUserData{
 		RolID:            12,
 		Name:             "Test",
 		PaternalSureName: "TestPS",
@@ -50,7 +47,76 @@ func TestCreateUser(t *testing.T) {
 		PasswordConfirm:  "pass1",
 	}
 
-	w, r := httptest.NewRecorder(), httptest.NewRequest(http.MethodPost, target, serialize(t, &data))
+	userUpdateEntityMock = entities.User{
+		UserID:           userUpdateModel.ID,
+		RolID:            zero.IntFrom(int64(userUpdateModel.RolID)),
+		Name:             userUpdateModel.Name,
+		PaternalSureName: userUpdateModel.PaternalSureName,
+		MaternalSureName: zero.NewString(userUpdateModel.MaternalSureName, true),
+		Email:            userUpdateModel.Email,
+		CreationDate:     time.Now(),
+		EditDate:         zero.NewTime(time.Now(), true),
+	}
+
+	updateData = map[string]interface{}{
+		"id":                1,
+		"name":              "Test",
+		"paternal_surename": "TestPS",
+		"maternal_surename": "TestMS",
+		"email":             "TestEmail@mail.com",
+		"rol_id":            12,
+	}
+
+	userUpdateModel = models.UpdateUserData{
+		ID:               1,
+		RolID:            12,
+		Name:             "Test",
+		PaternalSureName: "TestPS",
+		MaternalSureName: "TestMS",
+		Email:            "TestEmail@mail.com",
+	}
+
+	userRestorePassEntityMock = entities.User{
+		UserID:           1,
+		RolID:            zero.IntFrom(int64(1)),
+		Name:             "TestName",
+		PaternalSureName: "PaternalSureName",
+		MaternalSureName: zero.NewString("MaternalSureName", true),
+		Email:            "Email@mail.com",
+		CreationDate:     time.Now(),
+		EditDate:         zero.NewTime(time.Now(), true),
+		Password:         restorePasswordModel.Password,
+	}
+
+	restorePasswordData = map[string]interface{}{
+		"id":               1,
+		"password":         "TestPass",
+		"password_confirm": "TestPass",
+	}
+
+	restorePasswordModel = models.RestorePasswordData{
+		ID:              1,
+		Password:        "TestPass",
+		PasswordConfirm: "TestPass",
+	}
+)
+
+func setUp() {
+	router = mux.NewRouter()
+	validatorService = mocks.ValidatorServiceMock{}
+	userDao = mocks.UserDaoMock{}
+}
+
+/*
+	Create User
+*/
+
+// TestCreateUser valida el happy path del proceso de guardado de un usuario
+func TestCreateUser(t *testing.T) {
+	setUp()
+	target := "/admin/user/registry"
+
+	w, r := httptest.NewRecorder(), httptest.NewRequest(http.MethodPost, target, serialize(t, &creationData))
 
 	validatorService.On("Validate", userCreationModel).Return([]models.ErrorValidationDetail{}, true)
 	userDao.On("SaveFromModel", userCreationModel).Return(nil)
@@ -71,27 +137,8 @@ func TestCreateUser(t *testing.T) {
 func TestCreateUserValidatorError(t *testing.T) {
 	setUp()
 	target := "/admin/user/registry"
-	data := map[string]interface{}{
-		"name":              "",
-		"paternal_surename": "",
-		"maternal_surename": "TestMS",
-		"email":             "TestEmail@mail.com",
-		"rol_id":            12,
-		"password":          "pass1",
-		"password_confirm":  "pass1",
-	}
 
-	userCreationModel := models.CreateUserData{
-		RolID:            12,
-		Name:             "",
-		PaternalSureName: "",
-		MaternalSureName: "TestMS",
-		Email:            "TestEmail@mail.com",
-		Password:         "pass1",
-		PasswordConfirm:  "pass1",
-	}
-
-	w, r := httptest.NewRecorder(), httptest.NewRequest(http.MethodPost, target, serialize(t, &data))
+	w, r := httptest.NewRecorder(), httptest.NewRequest(http.MethodPost, target, serialize(t, &creationData))
 
 	validatorService.On("Validate", userCreationModel).Return([]models.ErrorValidationDetail{
 		{Field: "Name", Constraints: []string{
@@ -125,27 +172,8 @@ func TestCreateUserValidatorError(t *testing.T) {
 func TestCreateUserUserDaoError(t *testing.T) {
 	setUp()
 	target := "/admin/user/registry"
-	data := map[string]interface{}{
-		"name":              "Test",
-		"paternal_surename": "TestPS",
-		"maternal_surename": "TestMS",
-		"email":             "TestEmail@mail.com",
-		"rol_id":            12,
-		"password":          "pass1",
-		"password_confirm":  "pass1",
-	}
 
-	userCreationModel := models.CreateUserData{
-		RolID:            12,
-		Name:             "Test",
-		PaternalSureName: "TestPS",
-		MaternalSureName: "TestMS",
-		Email:            "TestEmail@mail.com",
-		Password:         "pass1",
-		PasswordConfirm:  "pass1",
-	}
-
-	w, r := httptest.NewRecorder(), httptest.NewRequest(http.MethodPost, target, serialize(t, &data))
+	w, r := httptest.NewRecorder(), httptest.NewRequest(http.MethodPost, target, serialize(t, &creationData))
 
 	validatorService.On("Validate", userCreationModel).Return([]models.ErrorValidationDetail{}, true)
 	userDao.On("SaveFromModel", userCreationModel).Return(fmt.Errorf("Un error en el UserDao"))
@@ -166,6 +194,206 @@ func TestCreateUserUserDaoError(t *testing.T) {
 	assert.Equal(t, "Hubo un error al guardar el usuario. Favor de contactar a soporte", res["message"].(string), "El mensaje no es el correcto")
 
 	assert.Equal(t, http.StatusInternalServerError, w.Code, "Debió regresar un status 500")
+}
+
+/*
+	Upsdate User
+*/
+
+// TestUpdateUser valida el happy path del proceso
+func TestUpdateUser(t *testing.T) {
+
+	setUp()
+	target := "/admin/user/update"
+
+	w, r := httptest.NewRecorder(), httptest.NewRequest(http.MethodPut, target, serialize(t, &updateData))
+
+	validatorService.On("Validate", userUpdateModel).Return([]models.ErrorValidationDetail{}, true)
+	userDao.On("FindUserByID", userUpdateModel.ID).Return(userUpdateEntityMock, nil)
+	userDao.On("Save", &userUpdateEntityMock).Return(nil)
+
+	controller := NewUserController(userDao, validatorService)
+
+	router.HandleFunc(target, controller.UpdateUser)
+
+	router.ServeHTTP(w, r)
+
+	validatorService.AssertExpectations(t)
+	userDao.AssertExpectations(t)
+
+	assert.Equal(t, http.StatusOK, w.Code, "Debió regresar un status 200")
+
+}
+
+// TestUpdateUserValidationError valida que se regresen los errores cuando la data no cumple lo requerido
+func TestUpdateUserValidationError(t *testing.T) {
+
+	setUp()
+	target := "/admin/user/update"
+
+	w, r := httptest.NewRecorder(), httptest.NewRequest(http.MethodPut, target, serialize(t, &updateData))
+
+	validatorService.On("Validate", userUpdateModel).Return([]models.ErrorValidationDetail{
+		{Field: "Name", Constraints: []string{
+			"required",
+		}},
+		{Field: "PaternalSureName", Constraints: []string{
+			"required",
+		}},
+	}, false)
+	// userDao.On("FindUserByID", userUpdateModel.ID).Return(userEntityMock, nil)
+	// userDao.On("Save", &userEntityMock).Return(nil)
+
+	controller := NewUserController(userDao, validatorService)
+
+	router.HandleFunc(target, controller.UpdateUser)
+
+	router.ServeHTTP(w, r)
+
+	validatorService.AssertExpectations(t)
+	userDao.AssertExpectations(t)
+
+	assert.Equal(t, http.StatusBadRequest, w.Code, "Debió regresar un status 400")
+
+}
+
+// TestUpdateUserUserNotFound valida que se mande error cuando el usuario no existe
+func TestUpdateUserUserNotFound(t *testing.T) {
+
+	setUp()
+	target := "/admin/user/update"
+
+	w, r := httptest.NewRecorder(), httptest.NewRequest(http.MethodPut, target, serialize(t, &updateData))
+
+	validatorService.On("Validate", userUpdateModel).Return([]models.ErrorValidationDetail{}, true)
+	userDao.On("FindUserByID", userUpdateModel.ID).Return(entities.User{}, sql.ErrNoRows)
+	// userDao.On("Save", &userEntityMock).Return(nil)
+
+	controller := NewUserController(userDao, validatorService)
+
+	router.HandleFunc(target, controller.UpdateUser)
+
+	router.ServeHTTP(w, r)
+
+	var res map[string]interface{}
+
+	json.NewDecoder(w.Body).Decode(&res)
+
+	validatorService.AssertExpectations(t)
+	userDao.AssertExpectations(t)
+
+	assert.Equal(t, "No se encontro el usuario que se quiere editar", res["message"].(string), "El mensaje no es el correcto")
+	assert.Equal(t, http.StatusNotFound, w.Code, "Debió regresar un status 404")
+
+}
+
+// TestUpdateUserUserDaoError valida la respuesta cuando un error inesperado sucede al buscar un usuario
+func TestUpdateUserUserDaoError(t *testing.T) {
+
+	setUp()
+	target := "/admin/user/update"
+
+	w, r := httptest.NewRecorder(), httptest.NewRequest(http.MethodPut, target, serialize(t, &updateData))
+
+	validatorService.On("Validate", userUpdateModel).Return([]models.ErrorValidationDetail{}, true)
+	userDao.On("FindUserByID", userUpdateModel.ID).Return(entities.User{}, fmt.Errorf("Un error al buscar el usuario"))
+	// userDao.On("Save", &userEntityMock).Return(nil)
+
+	controller := NewUserController(userDao, validatorService)
+
+	router.HandleFunc(target, controller.UpdateUser)
+
+	router.ServeHTTP(w, r)
+
+	var res map[string]interface{}
+
+	json.NewDecoder(w.Body).Decode(&res)
+
+	validatorService.AssertExpectations(t)
+	userDao.AssertExpectations(t)
+
+	assert.Equal(t, "Hubo un error inesperado al buscar al usuario", res["message"].(string), "El mensaje no es el correcto")
+	assert.Equal(t, http.StatusInternalServerError, w.Code, "Debió regresar un status 500")
+
+}
+
+// TestUpdateUserUserDaoSaveError valida se mande el mensaje correcto cuando hay un error al salvar el usuario
+func TestUpdateUserUserDaoSaveError(t *testing.T) {
+
+	setUp()
+	target := "/admin/user/update"
+
+	w, r := httptest.NewRecorder(), httptest.NewRequest(http.MethodPut, target, serialize(t, &updateData))
+
+	validatorService.On("Validate", userUpdateModel).Return([]models.ErrorValidationDetail{}, true)
+	userDao.On("FindUserByID", userUpdateModel.ID).Return(userUpdateEntityMock, nil)
+	userDao.On("Save", &userUpdateEntityMock).Return(fmt.Errorf("Un error en el metodo Save"))
+
+	controller := NewUserController(userDao, validatorService)
+
+	router.HandleFunc(target, controller.UpdateUser)
+
+	router.ServeHTTP(w, r)
+
+	var res map[string]interface{}
+
+	json.NewDecoder(w.Body).Decode(&res)
+
+	validatorService.AssertExpectations(t)
+	userDao.AssertExpectations(t)
+
+	assert.Equal(t, "Hubo un error inesperado al editar el usuario", res["message"].(string), "El mensaje no es el correcto")
+	assert.Equal(t, http.StatusInternalServerError, w.Code, "Debió regresar un status 500")
+
+}
+
+/*
+	Restore password
+*/
+
+func TestRestorePassword(t *testing.T) {
+	setUp()
+	target := "/admin/user/restore_password"
+
+	w, r := httptest.NewRecorder(), httptest.NewRequest(http.MethodPut, target, serialize(t, &restorePasswordData))
+
+	validatorService.On("Validate", restorePasswordModel).Return([]models.ErrorValidationDetail{}, true)
+	userDao.On("FindUserByID", restorePasswordModel.ID).Return(userRestorePassEntityMock, nil)
+	userDao.On("Save", &userRestorePassEntityMock).Return(nil)
+
+	controller := NewUserController(userDao, validatorService)
+
+	router.HandleFunc(target, controller.RestorePassword)
+
+	router.ServeHTTP(w, r)
+
+	validatorService.AssertExpectations(t)
+	userDao.AssertExpectations(t)
+
+	assert.Equal(t, http.StatusOK, w.Code, "Código http incorrecto")
+
+}
+
+func TestDeleteUser(t *testing.T) {
+	setUp()
+	endpoint := "/admin/user/delete/{idUser}"
+	target := "/admin/user/delete/1"
+	idUser := int32(1)
+
+	w, r := httptest.NewRecorder(), httptest.NewRequest(http.MethodDelete, target, serialize(t, &restorePasswordData))
+
+	userDao.On("Delete", idUser).Return(nil)
+
+	controller := NewUserController(userDao, validatorService)
+
+	router.HandleFunc(endpoint, controller.DeleteUser)
+
+	router.ServeHTTP(w, r)
+
+	validatorService.AssertExpectations(t)
+	userDao.AssertExpectations(t)
+
+	assert.Equal(t, http.StatusOK, w.Code, "Código http incorrecto")
 }
 
 func serialize(t *testing.T, i interface{}) *bytes.Buffer {

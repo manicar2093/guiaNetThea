412107289c636e1814b054811074c1709b5d1304
Manuel Carbajal <manicar2093@gmail.com>
2021-04-02 18:22:01 -0600

    Se modifica la validación de la fecha fin. Debe ser mayor a la fecha inicial

--- app/models/loginRegistry.model.go
+++ app/models/loginRegistry.model.go
@@ -4,7 +4,7 @@ import "time"
 
 type LoginRegistryData struct {
 	InitDate  time.Time `json:"initDate" validate:"required"`
-	FinalDate time.Time `json:"finalDate" validate:"required, gtefield=InitDate"`
+	FinalDate time.Time `json:"finalDate" validate:"required, gtfield=InitDate"`
 }
 
 func (l LoginRegistryData) GetValidableData() interface{} {
